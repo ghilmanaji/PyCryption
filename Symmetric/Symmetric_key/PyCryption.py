c3add66a2b21fb58f8687e77ab4243a79ae1f7da
ghilmanaji <139083228+ghilmanaji@users.noreply.github.com>
2023-07-24 22:03:05 +0700

    Update PyCryption.py

--- Symmetric/Symmetric_key/PyCryption.py
+++ Symmetric/Symmetric_key/PyCryption.py
@@ -35,3 +35,14 @@ for entry in os.listdir(basepath1):
                     encrypted = fernet.encrypt(original)
                     with open(path2_1+"/"+entry+'.aji', 'wb') as encrypted_file:
                         encrypted_file.write(encrypted)
+                if os.path.isdir(os.path.join(path1_1,entry)):
+                    path1_2=path1_1+"/"+entry
+                    path2_2=path2_1+"/"+entry
+                    os.mkdir(path2_2)
+                    for entry in os.listdir(path1_2):
+                        if os.path.isfile(os.path.join(path1_2, entry)):
+                            with open(path1_2+"/"+entry, 'rb') as file:
+                                original = file.read()
+                            encrypted = fernet.encrypt(original)
+                            with open(path2_2+"/"+entry+'.aji', 'wb') as encrypted_file:
+                                encrypted_file.write(encrypted)
