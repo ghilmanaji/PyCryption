61347071bc531af829f067358ec1781b32dcb744
Ghilman Aji <139083228+ghilmanaji@users.noreply.github.com>
2023-10-13 17:13:40 +0700

    Create DeCryption.py

---
+++ Symmetric/Symmetric_key/DeCryption.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from cryptography.fernet import Fernet
+
+with open('filekey.key', 'rb') as filekey:
+    key = filekey.read()
+fernet = Fernet(key)
+
+basepath1 ="Input_Decrypt"
+basepath2 ="Output_Decrypt"
+
+os.mkdir(basepath2)
+
+list_folder=[]
+for entry in os.listdir(basepath1):
+    if os.path.isfile(os.path.join(basepath1,entry)):
+        with open(basepath1+"/"+entry, 'rb') as file:
+            encrypted = file.read()
+        decrypted = fernet.decrypt(encrypted)
+        with open(basepath2+"/"+entry[0:-4], 'wb') as file:
+            file.write(decrypted)
+    if os.path.isdir(os.path.join(basepath1, entry)):
+        path1_1=basepath1+"/"+entry
+        path2_1=basepath2+"/"+entry
+        os.mkdir(path2_1)
+        for entry in os.listdir(path1_1):
+                if os.path.isfile(os.path.join(path1_1, entry)):
+                    with open(path1_1+"/"+entry, 'rb') as file:
+                        encrypted = file.read()
+                    decrypted = fernet.decrypt(encrypted)
+                    with open(path2_1+"/"+entry[0:-4], 'wb') as file:
+                        file.write(decrypted)
+                if os.path.isdir(os.path.join(path1_1,entry)):
+                    path1_2=path1_1+"/"+entry
+                    path2_2=path2_1+"/"+entry
+                    os.mkdir(path2_2)
+                    for entry in os.listdir(path1_2):
+                        if os.path.isfile(os.path.join(path1_2, entry)):
+                            with open(path1_2+"/"+entry, 'rb') as file:
+                                encrypted = file.read()
+                            decrypted = fernet.decrypt(encrypted)
+                            with open(path2_2+"/"+entry[0:-4], 'wb') as file:
+                                file.write(decrypted)
